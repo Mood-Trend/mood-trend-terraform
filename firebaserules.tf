1af815780f5e905a4d35b9a44903239a4f9bd92c
cobo <39579511+nzmgfdprs@users.noreply.github.com>
2023-10-23 21:40:17 +0900

    fix: ドキュメント通りの記載に変更

--- firebaserules.tf
+++ firebaserules.tf
@@ -1,5 +1,16 @@
 # Firebase Firestore Security Rules
-resource "google_firebaserules_ruleset" "firestore_rules" {
+resource "google_firebaserules_release" "primary" {
+  project      = var.project_id
+  name         = local.firebaserules_release_firestore_name
+  ruleset_name = "projects/${var.project_id}/rulesets/${google_firebaserules_ruleset.firestore.name}"
+
+  lifecycle {
+    replace_triggered_by = [
+      google_firebaserules_ruleset.firestore
+    ]
+  }
+}
+resource "google_firebaserules_ruleset" "firestore" {
   project = var.project_id
 
   source {
@@ -9,18 +20,22 @@ resource "google_firebaserules_ruleset" "firestore_rules" {
     }
   }
 }
-resource "google_firebaserules_release" "firestore_rules" {
-  project      = var.project_id
-  name         = local.firebaserules_release_firestore_name
-  ruleset_name = "projects/${var.project_id}/rulesets/${google_firebaserules_ruleset.firestore_rules.name}"
-
-  lifecycle {
-    replace_triggered_by = [google_firebaserules_ruleset.firestore_rules]
-  }
-}
 
 # Firebase Storage Security Rules
-resource "google_firebaserules_ruleset" "storage_rules" {
+resource "google_firebaserules_release" "primary" {
+  provider     = google-beta
+  project      = var.project_id
+  name         = "${local.firebaserules_release_storage_name}/buckets/${var.project_id}.appspot.com"
+  ruleset_name = "projects/${var.project_id}/rulesets/${google_firebaserules_ruleset.storage.name}"
+
+  lifecycle {
+    replace_triggered_by = [
+      google_firebaserules_ruleset.storage
+    ]
+  }
+}
+resource "google_firebaserules_ruleset" "storage" {
+  provider = google-beta
   project = var.project_id
 
   source {
@@ -28,14 +43,5 @@ resource "google_firebaserules_ruleset" "storage_rules" {
       content = file("firebaserules/storage.rules")
       name    = "storage.rules"
     }
-  }
-}
-resource "google_firebaserules_release" "storage_rules" {
-  project      = var.project_id
-  name         = local.firebaserules_release_storage_name
-  ruleset_name = "projects/${var.project_id}/rulesets/${google_firebaserules_ruleset.storage_rules.name}"
-
-  lifecycle {
-    replace_triggered_by = [google_firebaserules_ruleset.storage_rules]
   }
 }
