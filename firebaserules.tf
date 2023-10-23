1d720daca56a83c1e74112438d06d1a1fb1e5cc3
cobo <39579511+nzmgfdprs@users.noreply.github.com>
2023-10-23 21:05:31 +0900

    feat: 新規リソースとして定義

--- firebaserules.tf
+++ firebaserules.tf
@@ -1,5 +1,5 @@
 # Firebase Firestore Security Rules
-resource "google_firebaserules_ruleset" "firestore" {
+resource "google_firebaserules_ruleset" "firestore_rules" {
   project = var.project_id
 
   source {
@@ -9,18 +9,18 @@ resource "google_firebaserules_ruleset" "firestore" {
     }
   }
 }
-resource "google_firebaserules_release" "firestore" {
+resource "google_firebaserules_release" "firestore_rules" {
   project      = var.project_id
   name         = local.firebaserules_release_firestore_name
-  ruleset_name = google_firebaserules_ruleset.firestore.name
+  ruleset_name = "projects/${var.project_id}/rulesets/${google_firebaserules_ruleset.firestore_rules.name}"
 
   lifecycle {
-    replace_triggered_by = [google_firebaserules_ruleset.firestore]
+    replace_triggered_by = [google_firebaserules_ruleset.firestore_rules]
   }
 }
 
 # Firebase Storage Security Rules
-resource "google_firebaserules_ruleset" "storage" {
+resource "google_firebaserules_ruleset" "storage_rules" {
   project = var.project_id
 
   source {
@@ -30,12 +30,12 @@ resource "google_firebaserules_ruleset" "storage" {
     }
   }
 }
-resource "google_firebaserules_release" "storage" {
+resource "google_firebaserules_release" "storage_rules" {
   project      = var.project_id
   name         = local.firebaserules_release_storage_name
-  ruleset_name = google_firebaserules_ruleset.storage.name
+  ruleset_name = "projects/${var.project_id}/rulesets/${google_firebaserules_ruleset.storage_rules.name}"
 
   lifecycle {
-    replace_triggered_by = [google_firebaserules_ruleset.storage]
+    replace_triggered_by = [google_firebaserules_ruleset.storage_rules]
   }
 }
