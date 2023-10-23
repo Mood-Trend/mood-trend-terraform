1c3da5cd2a75d567c14de8f93aef2b4153de0cf5
cobo <39579511+nzmgfdprs@users.noreply.github.com>
2023-10-23 21:47:33 +0900

    style: 別ファイル化

--- firestore.tf
+++ firestore.tf
@@ -23,6 +23,29 @@ resource "google_firestore_database" "default" {
 #   depends_on  = [google_firestore_database.default]
 # }
 
+# Firebase Firestore Security Rules
+resource "google_firebaserules_release" "primary" {
+  project      = var.project_id
+  name         = local.firebaserules_release_firestore_name
+  ruleset_name = "projects/${var.project_id}/rulesets/${google_firebaserules_ruleset.firestore.name}"
+
+  lifecycle {
+    replace_triggered_by = [
+      google_firebaserules_ruleset.firestore
+    ]
+  }
+}
+resource "google_firebaserules_ruleset" "firestore" {
+  project = var.project_id
+
+  source {
+    files {
+      content = file("firebaserules/firestore.rules")
+      name    = "firestore.rules"
+    }
+  }
+}
+
 # Firebase Firestore Index
 # 今後書くコレクションのインデックスを定義する場合はコメントアウトを外して適宜編集してください
 # resource "google_firestore_index" "user-index" {
--- storage.tf
+++ storage.tf
@@ -1,26 +1,3 @@
-# Firebase Firestore Security Rules
-resource "google_firebaserules_release" "primary" {
-  project      = var.project_id
-  name         = local.firebaserules_release_firestore_name
-  ruleset_name = "projects/${var.project_id}/rulesets/${google_firebaserules_ruleset.firestore.name}"
-
-  lifecycle {
-    replace_triggered_by = [
-      google_firebaserules_ruleset.firestore
-    ]
-  }
-}
-resource "google_firebaserules_ruleset" "firestore" {
-  project = var.project_id
-
-  source {
-    files {
-      content = file("firebaserules/firestore.rules")
-      name    = "firestore.rules"
-    }
-  }
-}
-
 # Firebase Storage Security Rules
 resource "google_firebaserules_release" "primary" {
   provider     = google-beta
