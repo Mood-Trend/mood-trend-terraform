917c9dfb53d69f34b98ef1e7d3035e803c73b904
cobo <39579511+nzmgfdprs@users.noreply.github.com>
2023-12-23 01:42:04 +0900

    add: owner cobo

--- iam.tf
+++ iam.tf
@@ -1,3 +1,9 @@
+resource "google_project_iam_member" "owner_cobo" {
+  project = var.project_id
+  role    = "roles/owner"
+  member  = "user:${var.gcp_principal_cobo}"
+}
+
 resource "google_project_iam_member" "editor_midori" {
   project = var.project_id
   role    = "roles/editor"
