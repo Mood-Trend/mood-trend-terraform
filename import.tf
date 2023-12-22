3cf4b2948e287826b43d79722d3ce13a53727b83
cobo <39579511+nzmgfdprs@users.noreply.github.com>
2023-12-23 01:55:57 +0900

    fix: import block

--- import.tf
+++ import.tf
@@ -1,81 +1,81 @@
 # google_project_iam_member
 import {
-  id = "${var.project_id} roles/cloudasset.viewer serviceAccount:snyk-scan@mood-trend-dev.iam.gserviceaccount.com"
+  id = "${var.project_id} roles/cloudasset.viewer serviceAccount:snyk-scan@${var.project_id}.iam.gserviceaccount.com"
   to = google_project_iam_member.snyk_scan_cloudasset_editor
 }
 
 import {
-  id = "${var.project_id} roles/cloudbuild.builds.builder serviceAccount:cloud-scheduler-snyk-scan@mood-trend-dev.iam.gserviceaccount.com"
+  id = "${var.project_id} roles/cloudbuild.builds.builder serviceAccount:cloud-scheduler-snyk-scan@${var.project_id}.iam.gserviceaccount.com"
   to = google_project_iam_member.cloud_scheduler_snyk_scan_cloudbuild_builds_builder
 }
 
 import {
-  id = "${var.project_id} roles/cloudbuild.builds.builder serviceAccount:snyk-scan@mood-trend-dev.iam.gserviceaccount.com"
+  id = "${var.project_id} roles/cloudbuild.builds.builder serviceAccount:snyk-scan@${var.project_id}.iam.gserviceaccount.com"
   to = google_project_iam_member.snyk_scan_cloudbuild_builds_builder
 }
 
 import {
-  id = "${var.project_id} roles/cloudscheduler.jobRunner serviceAccount:cloud-scheduler-snyk-scan@mood-trend-dev.iam.gserviceaccount.com"
+  id = "${var.project_id} roles/cloudscheduler.jobRunner serviceAccount:cloud-scheduler-snyk-scan@${var.project_id}.iam.gserviceaccount.com"
   to = google_project_iam_member.cloud_scheduler_snyk_scan_cloudscheduler_jobRunner
 }
 
 import {
-  id = "${var.project_id} roles/editor serviceAccount:github-actions-functions@mood-trend-dev.iam.gserviceaccount.com"
+  id = "${var.project_id} roles/editor serviceAccount:github-actions-functions@${var.project_id}.iam.gserviceaccount.com"
   to = google_project_iam_member.github_actions_functions_editor
 }
 
 import {
-  id = "${var.project_id} roles/editor serviceAccount:github-actions-terraform@mood-trend-dev.iam.gserviceaccount.com"
+  id = "${var.project_id} roles/editor serviceAccount:github-actions-terraform@${var.project_id}.iam.gserviceaccount.com"
   to = google_project_iam_member.github_actions_terraform_editor
 }
 
 import {
-  id = "${var.project_id} roles/firebase.admin serviceAccount:github-actions-functions@mood-trend-dev.iam.gserviceaccount.com"
+  id = "${var.project_id} roles/firebase.admin serviceAccount:github-actions-functions@${var.project_id}.iam.gserviceaccount.com"
   to = google_project_iam_member.github_actions_functions_firebase_admin
 }
 
 import {
-  id = "${var.project_id} roles/firebase.admin serviceAccount:github-actions-terraform@mood-trend-dev.iam.gserviceaccount.com"
+  id = "${var.project_id} roles/firebase.admin serviceAccount:github-actions-terraform@${var.project_id}.iam.gserviceaccount.com"
   to = google_project_iam_member.github_actions_terraform_firebase_admin
 }
 
 import {
-  id = "${var.project_id} roles/firebase.sdkAdminServiceAgent serviceAccount:firebase-adminsdk-u21fw@mood-trend-dev.iam.gserviceaccount.com"
+  id = "${var.project_id} roles/firebase.sdkAdminServiceAgent serviceAccount:firebase-adminsdk-u21fw@${var.project_id}.iam.gserviceaccount.com"
   to = google_project_iam_member.firebase_adminsdk_u21fw_firebase_sdkAdminServiceAgent
 }
 
 import {
-  id = "${var.project_id} roles/firebaserules.admin serviceAccount:github-actions-terraform@mood-trend-dev.iam.gserviceaccount.com"
+  id = "${var.project_id} roles/firebaserules.admin serviceAccount:github-actions-terraform@${var.project_id}.iam.gserviceaccount.com"
   to = google_project_iam_member.github_actions_terraform_firebaserules_admin
 }
 
 import {
-  id = "${var.project_id} roles/iam.securityReviewer serviceAccount:snyk-scan@mood-trend-dev.iam.gserviceaccount.com"
+  id = "${var.project_id} roles/iam.securityReviewer serviceAccount:snyk-scan@${var.project_id}.iam.gserviceaccount.com"
   to = google_project_iam_member.snyk_scan_iam_securityReviewer
 }
 
 import {
-  id = "${var.project_id} roles/iam.serviceAccountTokenCreator serviceAccount:firebase-adminsdk-u21fw@mood-trend-dev.iam.gserviceaccount.com"
+  id = "${var.project_id} roles/iam.serviceAccountTokenCreator serviceAccount:firebase-adminsdk-u21fw@${var.project_id}.iam.gserviceaccount.com"
   to = google_project_iam_member.firebase_adminsdk_u21fw_iam_serviceAccountTokenCreator
 }
 
 import {
-  id = "${var.project_id} roles/iam.serviceAccountUser serviceAccount:cloud-scheduler-snyk-scan@mood-trend-dev.iam.gserviceaccount.com"
+  id = "${var.project_id} roles/iam.serviceAccountUser serviceAccount:cloud-scheduler-snyk-scan@${var.project_id}.iam.gserviceaccount.com"
   to = google_project_iam_member.cloud_scheduler_snyk_scan_iam_serviceAccountUser
 }
 
 import {
-  id = "${var.project_id} roles/iam.workloadIdentityUser serviceAccount:github-actions-functions@mood-trend-dev.iam.gserviceaccount.com"
+  id = "${var.project_id} roles/iam.workloadIdentityUser serviceAccount:github-actions-functions@${var.project_id}.iam.gserviceaccount.com"
   to = google_project_iam_member.github_actions_functions_iam_workloadIdentityUser
 }
 
 import {
-  id = "${var.project_id} roles/iam.workloadIdentityUser serviceAccount:github-actions-terraform@mood-trend-dev.iam.gserviceaccount.com"
+  id = "${var.project_id} roles/iam.workloadIdentityUser serviceAccount:github-actions-terraform@${var.project_id}.iam.gserviceaccount.com"
   to = google_project_iam_member.github_actions_terraform_iam_workloadIdentityUser
 }
 
 import {
-  id = "${var.project_id} roles/logging.configWriter serviceAccount:snyk-scan@mood-trend-dev.iam.gserviceaccount.com"
+  id = "${var.project_id} roles/logging.configWriter serviceAccount:snyk-scan@${var.project_id}.iam.gserviceaccount.com"
   to = google_project_iam_member.snyk_scan_logging_configWriter
 }
 
@@ -85,21 +85,21 @@ import {
 }
 
 import {
-  id = "${var.project_id} roles/resourcemanager.projectIamAdmin serviceAccount:github-actions-functions@mood-trend-dev.iam.gserviceaccount.com"
+  id = "${var.project_id} roles/resourcemanager.projectIamAdmin serviceAccount:github-actions-functions@${var.project_id}.iam.gserviceaccount.com"
   to = google_project_iam_member.github_actions_functions_resourcemanager_projectIamAdmin
 }
 
 import {
-  id = "${var.project_id} roles/resourcemanager.projectIamAdmin serviceAccount:github-actions-terraform@mood-trend-dev.iam.gserviceaccount.com"
+  id = "${var.project_id} roles/resourcemanager.projectIamAdmin serviceAccount:github-actions-terraform@${var.project_id}.iam.gserviceaccount.com"
   to = google_project_iam_member.github_actions_terraform_resourcemanager_projectIamAdmin
 }
 
 import {
-  id = "${var.project_id} roles/secretmanager.secretAccessor serviceAccount:snyk-scan@mood-trend-dev.iam.gserviceaccount.com"
+  id = "${var.project_id} roles/secretmanager.secretAccessor serviceAccount:snyk-scan@${var.project_id}.iam.gserviceaccount.com"
   to = google_project_iam_member.snyk_scan_secretmanager_secretAccessor
 }
 
 import {
-  id = "${var.project_id} roles/viewer serviceAccount:snyk-scan@mood-trend-dev.iam.gserviceaccount.com"
+  id = "${var.project_id} roles/viewer serviceAccount:snyk-scan@${var.project_id}.iam.gserviceaccount.com"
   to = google_project_iam_member.snyk_scan_viewer
 }
