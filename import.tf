6f67fd53dc7a69cd73c6323a3a1f863727154307
cobo <39579511+nzmgfdprs@users.noreply.github.com>
2024-02-07 23:21:43 +0900

    feature: import iam

--- import.tf
+++ import.tf
@@ -1,90 +1,10 @@
 # google_project_iam_member
 import {
-  id = "${var.project_id} roles/cloudasset.viewer serviceAccount:snyk-scan@${var.project_id}.iam.gserviceaccount.com"
-  to = google_project_iam_member.snyk_scan_cloudasset_editor
+  id = "${var.project_id} roles/firebase.sdkAdminServiceAgent serviceAccount:${var.firebase-adminsdk-service-account}@${var.project_id}.iam.gserviceaccount.com"
+  to = google_project_iam_member.firebase_adminsdk_u21fw_firebase_sdkAdminServiceAgent
 }
 
 import {
-  id = "${var.project_id} roles/cloudbuild.builds.builder serviceAccount:cloud-scheduler-snyk-scan@${var.project_id}.iam.gserviceaccount.com"
-  to = google_project_iam_member.cloud_scheduler_snyk_scan_cloudbuild_builds_builder
-}
-
-import {
-  id = "${var.project_id} roles/cloudbuild.builds.builder serviceAccount:snyk-scan@${var.project_id}.iam.gserviceaccount.com"
-  to = google_project_iam_member.snyk_scan_cloudbuild_builds_builder
-}
-
-import {
-  id = "${var.project_id} roles/cloudscheduler.jobRunner serviceAccount:cloud-scheduler-snyk-scan@${var.project_id}.iam.gserviceaccount.com"
-  to = google_project_iam_member.cloud_scheduler_snyk_scan_cloudscheduler_jobRunner
-}
-
-import {
-  id = "${var.project_id} roles/editor serviceAccount:github-actions-functions@${var.project_id}.iam.gserviceaccount.com"
-  to = google_project_iam_member.github_actions_functions_editor
-}
-
-import {
-  id = "${var.project_id} roles/editor serviceAccount:github-actions-terraform@${var.project_id}.iam.gserviceaccount.com"
-  to = google_project_iam_member.github_actions_terraform_editor
-}
-
-import {
-  id = "${var.project_id} roles/firebase.admin serviceAccount:github-actions-functions@${var.project_id}.iam.gserviceaccount.com"
-  to = google_project_iam_member.github_actions_functions_firebase_admin
-}
-
-import {
-  id = "${var.project_id} roles/firebase.admin serviceAccount:github-actions-terraform@${var.project_id}.iam.gserviceaccount.com"
-  to = google_project_iam_member.github_actions_terraform_firebase_admin
-}
-
-import {
-  id = "${var.project_id} roles/iam.securityReviewer serviceAccount:snyk-scan@${var.project_id}.iam.gserviceaccount.com"
-  to = google_project_iam_member.snyk_scan_iam_securityReviewer
-}
-
-import {
-  id = "${var.project_id} roles/iam.serviceAccountUser serviceAccount:cloud-scheduler-snyk-scan@${var.project_id}.iam.gserviceaccount.com"
-  to = google_project_iam_member.cloud_scheduler_snyk_scan_iam_serviceAccountUser
-}
-
-import {
-  id = "${var.project_id} roles/iam.workloadIdentityUser serviceAccount:github-actions-functions@${var.project_id}.iam.gserviceaccount.com"
-  to = google_project_iam_member.github_actions_functions_iam_workloadIdentityUser
-}
-
-import {
-  id = "${var.project_id} roles/iam.workloadIdentityUser serviceAccount:github-actions-terraform@${var.project_id}.iam.gserviceaccount.com"
-  to = google_project_iam_member.github_actions_terraform_iam_workloadIdentityUser
-}
-
-import {
-  id = "${var.project_id} roles/logging.configWriter serviceAccount:snyk-scan@${var.project_id}.iam.gserviceaccount.com"
-  to = google_project_iam_member.snyk_scan_logging_configWriter
-}
-
-import {
-  id = "${var.project_id} roles/owner user:${var.gcp_principal_cobo}"
-  to = google_project_iam_member.koborinai_owner
-}
-
-import {
-  id = "${var.project_id} roles/resourcemanager.projectIamAdmin serviceAccount:github-actions-functions@${var.project_id}.iam.gserviceaccount.com"
-  to = google_project_iam_member.github_actions_functions_resourcemanager_projectIamAdmin
-}
-
-import {
-  id = "${var.project_id} roles/resourcemanager.projectIamAdmin serviceAccount:github-actions-terraform@${var.project_id}.iam.gserviceaccount.com"
-  to = google_project_iam_member.github_actions_terraform_resourcemanager_projectIamAdmin
-}
-
-import {
-  id = "${var.project_id} roles/secretmanager.secretAccessor serviceAccount:snyk-scan@${var.project_id}.iam.gserviceaccount.com"
-  to = google_project_iam_member.snyk_scan_secretmanager_secretAccessor
-}
-
-import {
-  id = "${var.project_id} roles/viewer serviceAccount:snyk-scan@${var.project_id}.iam.gserviceaccount.com"
-  to = google_project_iam_member.snyk_scan_viewer
+  id = "${var.project_id} roles/iam.serviceAccountTokenCreator serviceAccount:${var.firebase-adminsdk-service-account}@${var.project_id}.iam.gserviceaccount.com"
+  to = google_project_iam_member.firebase_adminsdk_u21fw_iam_serviceAccountTokenCreator
 }
--- variables.tf
+++ variables.tf
@@ -34,3 +34,7 @@ variable "gcp_principal_satoru" {
 variable "gcp_principal_cobo" {
   type = string
 }
+
+variable "firebase-adminsdk-service-account" {
+  type = string
+}
