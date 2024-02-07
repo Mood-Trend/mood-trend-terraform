e6d67c1e7c4de517c34beab0e5c40c9f21494a2a
cobo <39579511+nzmgfdprs@users.noreply.github.com>
2024-02-08 00:18:44 +0900

    style: terraform fmt

--- import.tf
+++ import.tf
@@ -1,4 +1,4 @@
 import {
-  id = "${var.project_id}"
+  id = var.project_id
   to = google_app_engine_application.default
 }
--- storage.tf
+++ storage.tf
@@ -2,7 +2,7 @@ resource "google_app_engine_application" "default" {
   auth_domain    = "gmail.com"
   database_type  = "CLOUD_FIRESTORE"
   location_id    = "asia-northeast1"
-  project        = "${var.project_id}"
+  project        = var.project_id
   serving_status = "SERVING"
   feature_settings {
     split_health_checks = true
