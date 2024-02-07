24cfcae1b2eb4f069a82099f8d6d265ccd4bea3d
cobo <39579511+nzmgfdprs@users.noreply.github.com>
2024-02-07 23:39:02 +0900

    fix: import block

--- import.tf
+++ import.tf
@@ -1,10 +1,10 @@
 # google_project_iam_member
 import {
-  id = "${var.project_id} roles/firebase.sdkAdminServiceAgent serviceAccount:${var.firebase-adminsdk-service-account}@${var.project_id}.iam.gserviceaccount.com"
+  id = "${var.project_id} roles/firebase.sdkAdminServiceAgent serviceAccount:firebase-adminsdk-lnue0@${var.project_id}.iam.gserviceaccount.com"
   to = google_project_iam_member.firebase_adminsdk_u21fw_firebase_sdkAdminServiceAgent
 }
 
 import {
-  id = "${var.project_id} roles/iam.serviceAccountTokenCreator serviceAccount:${var.firebase-adminsdk-service-account}@${var.project_id}.iam.gserviceaccount.com"
+  id = "${var.project_id} roles/iam.serviceAccountTokenCreator serviceAccount:firebase-adminsdk-lnue0@${var.project_id}.iam.gserviceaccount.com"
   to = google_project_iam_member.firebase_adminsdk_u21fw_iam_serviceAccountTokenCreator
 }
